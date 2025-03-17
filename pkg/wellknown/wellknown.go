1bf3541d8321595926fcfe6100c06441322752d9
Md Suman Sarker <suman@appscode.com>
2025-03-17 13:24:52 +0600

    testing
    Signed-off-by: Md Suman Sarker <suman@appscode.com>

--- pkg/wellknown/wellknown.go
+++ pkg/wellknown/wellknown.go
@@ -80,6 +80,8 @@ const (
 	RedisProxy = "envoy.filters.network.redis_proxy"
 	// MySQLProxy network filter
 	MySQLProxy = "envoy.filters.network.mysql_proxy"
+	// MSSQL
+	MSSQLServerProxy = "envoy.filters.network.sql_server"
 	// ExternalAuthorization network filter
 	ExternalAuthorization = "envoy.filters.network.ext_authz"
 	// RoleBasedAccessControl network filter
